baf765f0b14a30f7c2c96d914ffba348f8d7889b
Dario Del Piano <dario@metacell.us>
2021-05-25 13:04:43 +0100

    #wrapper clean up

--- src/converter/converter.js
+++ src/converter/converter.js
@@ -1,43 +1,5 @@
 import {$Field} from "../model/utils";
-import {Graph}   from '../model/graphModel';
-import {Scaffold} from '../model/scaffoldModel';
 import {loadModel, fromJSON, fromJSONGenerated, fromJsonLD} from '../model/modelClasses';
-
-
-// Mirroring what the UI does, this part will be removed for the PR, it is intended just to study the code and model
-
-function convertToJson(content, name, extension, isBinary = true){
-    let model = loadModel(content, name, extension);
-    let _json_model = JSON.stringify(model, null, 4);
-    return _json_model;
-}
-
-function convertToJsonGenerated(content, name, extension, isBinary = true){
-    let model = loadModel(content, name, extension);
-    let graphData = fromJSON(model);
-    let result = JSON.stringify(graphData.toJSON(3, {
-        [$Field.border]   : 3,
-        [$Field.borders]  : 3,
-        [$Field.villus]   : 3,
-        [$Field.scaffolds]: 5
-    }), null, 2);
-    return result;
-}
-
-function convertToJsonLD(content, name, extension, isBinary = true){
-    let model = loadModel(content, name, extension);
-    let graphData = fromJSON(model);
-    let result = JSON.stringify(graphData.entitiesToJSONLD(), null, 2);
-    return result;
-}
-
-function convertToJsonLDFlattened(content, name, extension, callback){
-    let model = loadModel(content, name, extension);
-    let graphData = fromJSON(model);
-    graphData.entitiesToJSONLDFlat(callback);
-}
-
-// ####  remove end here  #####
 
 
 function fromXLSXToJson(data) {
@@ -70,14 +32,7 @@ async function fromJsonLDToFlattened(data, _callback) {
 }
 
 
-
-
 exports.fromXLSXToJson = fromXLSXToJson;
 exports.fromJsonToGenerated = fromJsonToGenerated;
 exports.fromGeneratedToJsonLD = fromGeneratedToJsonLD;
 exports.fromJsonLDToFlattened = fromJsonLDToFlattened;
-
-exports.convertToJson = convertToJson;
-exports.convertToJsonLD = convertToJsonLD;
-exports.convertToJsonGenerated = convertToJsonGenerated;
-exports.convertToJsonLDFlattened = convertToJsonLDFlattened;
--- wrapper/bin/converter.js
+++ wrapper/bin/converter.js
@@ -1,13 +1,6 @@
 #!/usr/bin/env node
 
-global.self = {};
-global.window = {};
-
-const fs = require('fs');
-const util = require("util");
-const axios = require("axios");
 const yargs = require("yargs");
-const converter = require('../../dist/converter');
 const ConversionHandler = require('./model/filehandler');
 
 const options = yargs
@@ -18,62 +11,3 @@ const options = yargs
 
 to_convert = new ConversionHandler(options.m, options.i);
 to_convert.convertAll();
-
-// let _path = options.i
-// readTextFile(_path)
-
-
-
-
-function readTextFile(file)
-{
-    let _xlsx = fs.readFileSync(file, 'binary');
-    let results1 = converter.fromXLSXToJson(_xlsx);
-    fs.writeFileSync("model.json", results1);
-
-    let _json = fs.readFileSync("model.json", 'binary');
-    let results2 = converter.fromJsonToGenerated(_json);
-    fs.writeFileSync("model-generated.json", results2);
-
-    let _jsonLD = converter.convertToJsonLD(_xlsx, ".xlsx", "xlsx");
-    fs.writeFileSync("model2.jsonLD", _jsonLD);
-
-    let _generated = fs.readFileSync("model-generated.json", 'binary');
-    let results3 = converter.fromGeneratedToJsonLD(_generated);
-    fs.writeFileSync("model.jsonLD", results3);
-
-    const _callback = function callback(res) {
-        let _flattened = JSON.stringify(res, null, 2);
-        fs.writeFileSync("model-flattened.jsonLD", _flattened);
-    };
-
-    let _jsonLD2 = fs.readFileSync("model2.jsonLD", 'binary');
-    converter.fromJsonLDToFlattened(_jsonLD2, _callback);
-
-    const _callback2 = function callback(res) {
-        let _flattened = JSON.stringify(res, null, 2);
-        fs.writeFileSync("model-flattened2.jsonLD", _flattened);
-    };
-
-    converter.convertToJsonLDFlattened(_xlsx, ".xlsx", "xlsx", _callback2);
-
-
-    // fs.readFile(file, 'binary', function(err, data) {
-    //     if (err) throw err;
-    //     _json = converter.convertToJson(data, ".xlsx", "xlsx")
-    //     fs.writeFileSync("model.json", _json);
-
-    //     _generated = converter.convertToJsonGenerated(data, ".xlsx", "xlsx");
-    //     fs.writeFileSync("model-generated.json", _generated);
-
-    //     _jsonLD = converter.convertToJsonLD(data, ".xlsx", "xlsx");
-    //     fs.writeFileSync("model.jsonLD", _jsonLD);
-
-    //     const _callback = function callback(res) {
-    //         let _flattened = JSON.stringify(res, null, 2);
-    //         fs.writeFileSync("model-flattened.jsonLD", _flattened);
-    //     };
-    //     // converter.convertToJsonLDFlattened(data, ".xlsx", "xlsx", _callback);
-    //     converter.convertToJsonLDFlattened2(fs.readFileSync("model-generated.json", 'binary'), _callback);
-    // });
-}
