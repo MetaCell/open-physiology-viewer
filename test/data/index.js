77ce5d791ed70853622036ae33812bf24a75fd0c
enicolasgomez <enicolasgomez@gmail.com>
2022-04-11 17:39:48 -0300

    Added all test cases to index fail

--- test/data/index.js
+++ test/data/index.js
@@ -1,4 +1,76 @@
-import basalGanglia from './basalGanglia.json'
-import basic from './basic.json'
+import basicLyphTypes from './basicLyphTypes.json'                   
+import keastSpinalTest from './keastSpinalTest.json'
+import basalGanglia from './basalGanglia.json'                      
+import basicLyphWithNoAxis from './basicLyphWithNoAxis.json'               
+import keastSpinalTestScaffold from './keastSpinalTestScaffold.json'
+import basalGangliaAuto from './basalGangliaAuto.json'                  
+import basicSharedNodes from './basicSharedNodes.json'                  
+import neuron from './neuron.json'
+import basalGangliaInternal from './basalGangliaInternal.json'              
+import babasicVillussic from './basicVillus.json'                       
+import neuronTemplate from './neuronTemplate.json'
+import basic from './basic.json'                             
+import bolserLewis from './bolserLewis.json'                       
+import neuronTemplateRegion from './neuronTemplateRegion.json'
+import basicChainHousedInOneLyph from './basicChainHousedInOneLyph.json'         
+import fullBody from './fullBody.json'                          
+import neuronTree from './neuronTree.json'
+import basicChainWireConflict from './basicChainWireConflict.json'                                        
+import neuronTreeWithLevels from './neuronTreeWithLevels.json'
+import basicChainWithNestedLevels from './basicChainWithNestedLevels.json'        
+import keastNamespaces from './keastNamespaces.json'                   
+import neurulatorTestShortUotBag from './neurulatorTestShortUotBag.json'
+import basicChainsInGroup from './basicChainsInGroup.json'             
+import keastSameLyphHousing from './keastSameLyphHousing.json'              
+import respiratory from './respiratory.json'
+import basicHostedNode from './basicHostedNode.json'                   
+import keastSpinal from './keastSpinal.json'                       
+import respiratoryInternalLyphsInLayers from './respiratoryInternalLyphsInLayers.json'
+import basicHousedTree from './basicHousedTree.json'                   
+import bakeastSpinalFullsic from './keastSpinalFull.json'                   
+import uot from './uot.json'
+import basicJointTrees from './basicJointTrees.json'                    
+import uotWithChannels from './uotWithChannels.json'
+import basicLinkTypes from './basicLinkTypes.json'                    
+import basicLyphOnBorder from './basicLyphOnBorder.json'                 
 
-export const test_data = { basalGanglia, basic } 
+export const test_data = { 
+  basicLyphTypes,                    
+  keastSpinalTest,
+  basalGanglia,                      
+  basicLyphWithNoAxis,               
+  keastSpinalTestScaffold,
+  basalGangliaAuto,   
+  babasicVillussic, 
+  basicSharedNodes,               
+  neuron,
+  basalGangliaInternal,              
+  basicVillus,    
+  neuronTemplate,
+  basic,             
+  bolserLewis,              
+  neuronTemplateRegion,
+  basicChainHousedInOneLyph,        
+  fullBody,     
+  neuronTree,
+  basicChainWireConflict,         
+  index,                               
+  neuronTreeWithLevels,
+  basicChainWithNestedLevels,      
+  keastNamespaces,           
+  neurulatorTestShortUotBag,
+  basicChainsInGroup,            
+  keastSameLyphHousing,         
+  respiratory,
+  basicHostedNode,                
+  keastSpinal,                   
+  respiratoryInternalLyphsInLayers,
+  basicHousedTree,               
+  keastSpinalFull,   
+  bakeastSpinalFullsic,
+  uot,
+  basicJointTrees,            
+  uotWithChannels,
+  basicLinkTypes,        
+  basicLyphOnBorder
+ } 
