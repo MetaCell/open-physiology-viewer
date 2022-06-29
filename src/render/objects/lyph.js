d024c1b4ef97e751a00b27efda19b80250a45648
enicolasgomez <enicolasgomez@gmail.com>
2022-06-29 17:20:46 -0300

    Extended lyph rendering

--- src/render/objects/lyph.js
+++ src/render/objects/lyph.js
@@ -4,17 +4,40 @@ import { reducerTypes } from '../reducer';
 import { ThreeDFactory } from '../threeDFactory'; 
 import { MaterialFactory } from '../materialFactory';
 
+const LYPH_TOPOLOGY = Object.freeze({
+  CYST: 'DASHED',
+  BAG: 'THICK',
+  BAG2: 'LINK'
+})
+
+LYPH_TOPOLOGY['BAG-'] = 'BAG-';
+LYPH_TOPOLOGY['BAG+'] = 'BAG+';
+
 export class Lyph extends objectBase
 {
   _layers = [];
-
+  _topology ;
+  _radialTypes = [false, false];
   constructor(json, reducer)
   {
     super(json, objectTypes.lyphs, reducer);
     this.width = this._json.scale?.width ;
     this.height = this._json.scale?.height ;
+    this.radius = this.height / 8 ;
     this.color = this._json.color ;
+    this._topology = this._json.topology;
     this._groupped = true ;
+    initRadialTypes();
+  }
+
+  initRadialTypes() {
+      switch (this._topology) {
+          case LYPH_TOPOLOGY.CYST   : this._radialTypes [true, true]; break;
+          case LYPH_TOPOLOGY.BAG    : this._radialTypes[0] = true; break;
+          case LYPH_TOPOLOGY.BAG2   : this._radialTypes[1] = true; break;
+          case LYPH_TOPOLOGY["BAG-"]: this._radialTypes[0] = true; break;
+          case LYPH_TOPOLOGY["BAG+"]: this._radialTypes[1] = true; break;
+      }
   }
 
   merge() {
@@ -35,9 +58,11 @@ export class Lyph extends objectBase
   }
 
   render() {
-    //const group = new THREE.Group();
+    const params = { color: this._color, polygonOffsetFactor: this._polygonOffsetFactor} ;
+    //thickness, height, radius, top, bottom
+    const lyph = lyphShape([this._width, this._height, this._radius, ...this._radialTypes]) ;
+    let geometry = ThreeDFactory.createMeshWithBorder(lyph, params);
 
-    const geometry = ThreeDFactory.createBoxGeometry(this._width, this._height);
     const layers = [];
 
     const material = MaterialFactory.createMeshLambertMaterial({
