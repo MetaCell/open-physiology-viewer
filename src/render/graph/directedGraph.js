6643c388d70e4674bd34cc59627fbe26824a7d27
enicolasgomez <enicolasgomez@gmail.com>
2022-10-21 18:43:16 -0300

    Ready to start working on too map

--- src/render/graph/directedGraph.js
+++ src/render/graph/directedGraph.js
@@ -3,6 +3,13 @@ import dagre from "cytoscape-dagre";
 
 cytoscape.use(dagre);
 
+function getPropertyOrValue(o, prop)
+{
+  if(o[prop])
+    return o[prop];
+  else return o ;
+}
+
 export function nodeFromGeneratedModel(o)
 {
   return {
@@ -14,16 +21,20 @@ export function nodeFromGeneratedModel(o)
 
 export function linkFromGeneratedModel(o)
 {
+  if (!o._generatedModel.source || !o._generatedModel.target)
+    return;
+    
   return {
     data: {
       id: o._id,
-      source: o._generatedModel.source.id,
-      target: o._generatedModel.target.id,
+      source: getPropertyOrValue(o._generatedModel.source, 'id'),
+      target: getPropertyOrValue(o._generatedModel.target, 'id'),
       weight: o._generatedModel.length, 
       minLen: o._generatedModel.length, 
     }
   }
 }
+
 function createElements(nodes, edges)
 {
   return {
@@ -48,32 +59,9 @@ export class DirectedGraph {
 
   runLayout() {
     this._graph.layout({
-      name: 'dagre',
-      // dagre algo options, uses default value on undefined
-      nodeSep: undefined, // the separation between adjacent nodes in the same rank
-      edgeSep: undefined, // the separation between adjacent edges in the same rank
-      rankSep: undefined, // the separation between each rank in the layout
-      rankDir: undefined, // 'TB' for top to bottom flow, 'LR' for left to right,
-      align: undefined,  // alignment for rank nodes. Can be 'UL', 'UR', 'DL', or 'DR', where U = up, D = down, L = left, and R = right
-      acyclicer: undefined, // If set to 'greedy', uses a greedy heuristic for finding a feedback arc set for a graph.
-                            // A feedback arc set is a set of edges that can be removed to make a graph acyclic.
-      ranker: undefined, // Type of algorithm to assign a rank to each node in the input graph. Possible values: 'network-simplex', 'tight-tree' or 'longest-path'
-      minLen: function( edge ){ return 1; }, // number of ranks to keep between the source and target of the edge
-      edgeWeight: function( edge ){ return 1; }, // higher weight edges are generally made shorter and straighter than lower weight edges
-    
-      // general layout options
-      fit: true, // whether to fit to viewport
-      padding: 30, // fit padding
-      spacingFactor: undefined, // Applies a multiplicative factor (>0) to expand or compress the overall area that the nodes take up
-      nodeDimensionsIncludeLabels: false, // whether labels should be included in determining the space used by a node
-      animate: false, // whether to transition the node positions
-      animateFilter: function( node, i ){ return true; }, // whether to animate specific nodes when animation is on; non-animated nodes immediately go to their final positions
-      animationDuration: 500, // duration of animation in ms if enabled
-      animationEasing: undefined, // easing of animation if enabled
-      boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
-      transform: function( node, pos ){ return pos; }, // a function that applies a transform to the final node position
-      ready: function(){}, // on layoutready
-      stop: function(){} // on layoutstop
+      name: "cose",
+      nodeDimensionsIncludeLabels: false,
+      animate: false 
     }).run();
   }
 }
