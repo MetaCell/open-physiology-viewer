580937dad759c58838942faaef952f5a99e085c5
enicolasgomez <enicolasgomez@gmail.com>
2022-10-21 15:03:14 -0300

    Added graph

--- src/test-app/app.js
+++ src/test-app/app.js
@@ -34,7 +34,7 @@ import {
 } from '../model/index';
 
 import 'hammerjs';
-import initModel from '../../test/data/basicTemplateAsInternalLyphInLayer.json'
+import initModel from '../../test/data/graph.json'
 //import initModel from '../../test/data/wbgkCardiac';
 
 import 'font-awesome/css/font-awesome.css';
