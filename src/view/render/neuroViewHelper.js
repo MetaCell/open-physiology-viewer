bec59c71fd7a517f4d97c043a779855cd978fb63
enicolasgomez <enicolasgomez@gmail.com>
2023-04-10 21:27:09 -0300

    fixed svg exporter

--- src/view/render/neuroViewHelper.js
+++ src/view/render/neuroViewHelper.js
@@ -1,6 +1,6 @@
 import orthogonalConnector2 from "./orthogonalConnector2";
 import { dia, shapes } from 'jointjs';
-import { combineLatestAll } from "rxjs";
+import { combineLatestAll, generate } from "rxjs";
 
 function extractVerticesFromPath(path)
 {
@@ -20,12 +20,79 @@ function extractVerticesFromPath(path)
   return vertices ;
 }
 
+function pointsToSVGPath(points, deltaX) {
+  if (!points || points.length === 0) {
+    return '';
+  }
+
+  const pathCommands = [];
+
+  // Move to the first point
+  const firstPoint = points[0];
+  pathCommands.push(`M ${firstPoint.x + deltaX} ${firstPoint.y}`);
+
+  // Create line commands for the rest of the points
+  for (let i = 1; i < points.length; i++) {
+    const point = points[i];
+    pathCommands.push(`L ${point.x+deltaX} ${point.y}`);
+  }
+
+  // Join the commands into a single string
+  const pathData = pathCommands.join(' ');
+
+  return pathData;
+}
+
+function exportToSVG(graphJSONCells, jointGraph, paper, paperWidth, paperHeight) {
+  // Create an SVG element for the exported content
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+  svg.setAttribute('width', paperWidth);
+  svg.setAttribute('height', paperHeight);
+  const deltaX = 500 ;
+
+  // Iterate through all the elements in the JointJS graph
+  graphJSONCells.forEach((cellData) => {
+    const cell = jointGraph.getCell(cellData.id);
+
+    if (cellData.type === 'standard.Rectangle') {
+      const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+      rect.setAttribute('x', cellData.position.x + deltaX);
+      rect.setAttribute('y', cellData.position.y);
+      rect.setAttribute('width', cellData.size.width);
+      rect.setAttribute('height', cellData.size.height);
+      rect.setAttribute('fill', cellData.attrs.body.fill);
+      rect.setAttribute('stroke', cellData.attrs.body.stroke);
+      rect.setAttribute('stroke-width', cellData.attrs.body.strokeWidth);
+
+      svg.appendChild(rect);
+    } else if (cellData.type === 'standard.Link') {
+      const link = paper.findViewByModel(cell);
+      const points = link.path.toPoints();
+      const pathData = pointsToSVGPath(points[0], deltaX);
+
+      const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+      path.setAttribute('d', pathData);
+      path.setAttribute('fill', 'none');
+      path.setAttribute('stroke', cellData.attrs.line.stroke);
+      path.setAttribute('stroke-width', 2);
+
+      svg.appendChild(path);
+    }
+  });
+
+  // Serialize the SVG content to a string
+  const serializer = new XMLSerializer();
+  const svgString = serializer.serializeToString(svg);
+
+  // Return the serialized SVG string
+  return svgString;
+}
+
 export function orthogonalLayout(links, nodes, left, top, width, height)
 {
   const graph = new dia.Graph();
   const linkVertices = {};
-  const obstacles = [];
-  const cells = [];
 
   const el = document.createElement('div');
   el.style.width = width + 'px';
@@ -35,25 +102,31 @@ export function orthogonalLayout(links, nodes, left, top, width, height)
     el: el,
     width: width,
     height: height,
-    gridSize: 100,
-    async: false,
-    model: graph
+    gridSize: 1,
+    model: graph,
+    defaultConnector: { name: 'rounded' },
+    defaultConnectionPoint: { name: 'boundary', args: { extrapolate: true } },
+    interactive: true
   });
   //obstacles, anything not a lyph and orphaned
 
   nodes.forEach( node => {
     const nodeModel = new shapes.standard.Rectangle({
       id: node.id,
-      position: { x: node.x, y: node.y },
+      position: { x: node.x + left, y: node.y + top },
       size: { 
         width: node.scale.width
         , height: node.scale.height
+      },
+      attrs: {
+          body: { fill: 'blue', stroke: 'black', strokeWidth: 2 },
+          label: { text: node.id, fill: 'white', fontWeight: 'bold' }
       }
     });
   
-    obstacles.push(nodeModel);
+    graph.addCell(nodeModel);
   });
-  
+
   links.forEach( link => {
 
     if (link.points?.length > 0)
@@ -63,29 +136,19 @@ export function orthogonalLayout(links, nodes, left, top, width, height)
       const tx = link.points[link.points.length-1].x ;
       const ty = link.points[link.points.length-1].y ;
   
-      const source = new shapes.standard.Rectangle({
-        position: { x: sx, y: sy },
-        size: { width: 0.01, height: 0.01 },
-      });
-      cells.push(source);
-      const target = new shapes.standard.Rectangle({
-        position: { x: tx, y: ty },
-        size: { width: 0.01, height: 0.01 },
-      });
-      cells.push(target);
-      const linkModel = new shapes.standard.Link({
+      const connection = new shapes.standard.Link({
         id: link.id,
-        source: { id: source.id },
-        target: { id: target.id },
-        router: { name: 'manhattan' },
-        connector : { name : 'jumpover'}
+        source: { x: sx, y: sy },
+        target: { x: tx, y: ty },
+        connector: { name: 'rounded' },
+        router: { name: 'manhattan', args: { obstacles: graph.getElements() } },
+        attrs: {
+            line: { stroke: 'red', strokeWidth: 2, targetMarker: { type: 'path', d: 'M 10 -5 0 0 10 5 z' } }
+        }
       });
-      cells.push(linkModel);
+      graph.addCell(connection);
     }
   })
-
-  graph//.addCells(obstacles)
-  .addCells(cells);
 
   // Wait for the routing update to complete
   const json = graph.toJSON();
@@ -99,6 +162,9 @@ export function orthogonalLayout(links, nodes, left, top, width, height)
       }
     }
   });
+
+  const svg = exportToSVG(json.cells, graph, paper, width, height);
+  console.log(svg);
 
   return linkVertices ;
 }
